4256e166e39cf0632e27414c38f6918f3f7fb3aa
KHE <99ant@yonsei.ac.kr>
2025-04-07 17:20:30 +0900

    init

---
+++ backend/src/main/java/org/example/hakple/HakpleApplication.java
@@ -0,0 +1,13 @@
+package org.example.hakple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HakpleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HakpleApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/org/example/hakple/HakpleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hakple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HakpleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
